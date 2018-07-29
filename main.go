9c81b4accc873356dfcbbe32c5cdb937d3abe253
cpluspluscrowe <ccrowe@unomaha.edu>
2018-07-29 22:12:58 +0000

    added liking tweets

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"fmt"
+	"github.com/amit-lulla/twitterapi"
+	"github.com/deckarep/golang-set"
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/dghubble/oauth1"
 	"net/http"
 	"os"
-	"strings"
 	"time"
 )
+
+var tweetIds = mapset.NewSet()
 
 func doEvery(d time.Duration, f func()) {
 	for _ = range time.Tick(d) {
@@ -16,14 +20,19 @@ func doEvery(d time.Duration, f func()) {
 }
 
 func main() {
-	doEvery(500000*time.Millisecond, Retweet)
+	doEvery(200000*time.Millisecond, Retweet)
 }
 
 func Retweet() {
-	Search("#Golang")
+	tweetIds = SearchAndFavorite("Kotlin", tweetIds)
+	tweetIds = SearchAndFavorite("Golang", tweetIds)
+	tweetIds = SearchAndFavorite("Scala", tweetIds)
+	tweetIds = SearchAndFavorite("Spark", tweetIds)
+	tweetIds = SearchAndFavorite("Python", tweetIds)
+	fmt.Println()
 }
 
-func Search(look4 string) {
+func SearchAndFavorite(look4 string, tweetIds mapset.Set) mapset.Set {
 	twitterClient := getTwitterClient()
 	search, _, err := twitterClient.Search.Tweets(&twitter.SearchTweetParams{
 		Query: look4,
@@ -31,14 +40,18 @@ func Search(look4 string) {
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println("Searching")
 	for _, tweet := range search.Statuses {
 		tweet_id := tweet.ID
-		text := tweet.Text
-		if strings.Contains(text, "#Golang") {
-			twitterClient.Statuses.Retweet(tweet_id, &twitter.StatusRetweetParams{})
+		//text := tweet.Text
+		//twitterClient.Statuses.Retweet(tweet_id, &twitter.StatusRetweetParams{})
+		if !tweetIds.Contains(tweet_id) {
+			fmt.Println(tweet_id)
+			//Favorite(tweet_id)
 		}
-		break
+		tweetIds.Add(tweet_id)
 	}
+	return tweetIds
 }
 
 func getClient() *http.Client {
@@ -56,4 +69,20 @@ func getTwitterClient() *twitter.Client {
 	httpClient := getClient()
 	client := twitter.NewClient(httpClient)
 	return client
+}
+
+func Favorite(tweetId int64) {
+	consumerKey := os.Getenv("consumerKey")
+	consumerSecret := os.Getenv("consumerSecret")
+	accessToken := os.Getenv("accessToken")
+	accessSecret := os.Getenv("accessSecret")
+
+	twitterapi.SetConsumerKey(consumerKey)
+	twitterapi.SetConsumerSecret(consumerSecret)
+	api := twitterapi.NewTwitterApi(accessToken, accessSecret)
+	_, err := api.Favorite(tweetId)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
